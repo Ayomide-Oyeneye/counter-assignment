96aea3c62a587a8634ec75614f181dfe6921361b
Ayomide-Oyeneye <nickbelson11@gmail.com>
2023-09-08 17:17:03 -0700

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,43 @@ import {RotateCcw } from 'react-feather';
 function App() {
   const [count, setCount] = useState(0)
   const [me,  setMe]=useState(0)
-
+  const [durrent,  serDurrent]=useState('Current value:')
+  function textChange(){
+    serDurrent('u inc')
+  }
+  function uptet(){
+    setCount((me) => me + 1)
+  }
   return (
     <>
       <div
         className="card"
       >
-        <p className='current'>Current value:</p>
+        <p className='current'>{durrent}</p>
         <a style={{ fontSize: 70, color: "#4517CF", fontWeight: "700" }} className='numChange'>
           {count}
         </a>
         <div className='btn' >
-        <button onClick={() => setCount((me) => me + 1)}>
+        <button onClick={() =>{ setCount((me) => me + 1);serDurrent('You increased the counter by 1')}}>
         < ChevronUp />{" "}
           </button>
-          <button onClick={() => setCount((me) => me + 10)}>
+          <button onClick={() => {setCount((me) => me + 10);serDurrent('You increased the counter by 10')}}>
           < ChevronsUp />{" "}
           </button>
-          <button onClick={() => setCount((me) => (me = 0))}>
+          <button onClick={() => {setCount((me) => (me = 0));serDurrent('Current value:')}}>
           < RotateCcw />
           </button>
           <button
             onClick={() =>
-              setCount((count) => (count = Math.floor(Math.random() * 100)))
+              {setCount((count) => (count = Math.floor(Math.random() * 100)));;serDurrent('You are randomly choosing values ')}
             }
           >
             <Hash />{" "}
           </button>
-          <button onClick={() => setCount((me) => me - 10)}>
+          <button onClick={() => {setCount((me) => me - 10);;serDurrent('You decreased the counter by 1')}}>
           < ChevronsDown />
           </button>
-          <button onClick={() => setCount((me) => me - 1)}>
+          <button onClick={() => {setCount((me) => me - 1);;serDurrent('You decreased the counter by 10')}}>
           < ChevronDown />{" "}
           </button>
         </div>
